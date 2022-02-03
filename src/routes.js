cee5fc0f2c0a791d856c422481f1608bcf8dbad4
diegolimavoll <diego.lima@vollsolutions.com.br>
2022-02-03 07:01:38 -0300

    apliquei a Errorpage no router

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/home/'
 import Links from './pages/links'
+import Error from './pages/error/Error'
 
 export default RouterApp =>{
     return(
         <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='myLinks' element={<Links/>} />
+            <Route path='myLinks' element={<Links/>}/>
+            <Route path='*' element={<Error/>}/> {/* quando usamos o * react-router entede com page not found */}
         </Routes>
         </BrowserRouter>
     )
