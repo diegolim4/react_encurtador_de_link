423c4cb975a1ae16c93b4a38578240dce1bce8ae
diegolimavoll <diego.lima@vollsolutions.com.br>
2022-02-03 07:25:46 -0300

    aplicando useState

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import {FiLink} from 'react-icons/fi'
 import './home.css'
 
 import Menu from '../../components/Menu/menu';
 
 function Home() {
+
+  const [link, setLink] = useState('')
+
   return (
     <div className="container-home">
       <div className="logo">
@@ -15,7 +19,11 @@ function Home() {
       <div className="area-input">
         <div>
           <FiLink size={24} color='#FFF'/>
-          <input placeholder='Cole seu link aqui...'/>  
+          
+          <input placeholder='Cole seu link aqui...'
+            value={link}
+            onChange={(e)=> setLink(e.target.value)}
+          />  
         </div>
           
         <button>Encurtar Link</button>
