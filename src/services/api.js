0b053979c8f833d6dff54245408ac325e2350761
Diego Lima <lima.j.diego@gmail.com>
2022-02-07 20:26:21 -0300

    Criado o serviço

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
-
+const axios = require('axios')
 
 const key = 'cf4544e59effa73107937c440769fef3582f8388'
+
 
 const api = axios.create({
     baseURL: 'https://api-ssl.bitly.com/v4',
     headers:{
-        'Autorization': `Bearer ${key}`
+        'Authorization': `Bearer ${key}`
     }
 })
 
