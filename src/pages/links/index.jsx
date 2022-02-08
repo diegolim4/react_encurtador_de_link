0df794220e1e9fd0eb7b074ded72f3a086ecce32
Diego Lima <lima.j.diego@gmail.com>
2022-02-08 08:16:57 -0300

    Trazendo a função de salvar os links e os salvando na page de links salvos

--- src/pages/links/index.jsx
+++ src/pages/links/index.jsx
@@ -1,8 +1,27 @@
 import './links.css'
+import { useState, useEffect} from 'react'
 import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
+import { getLinksSave } from '../../services/storeLinks'
+
 export default Links =>{
+    const [myLinks, setMyLinks] = useState([])
+    const [data, setData] = useState({})
+    const [showModal, setShowModal] = useState(false)
+
+    useEffect(()=> {
+        async function getLinks(){
+            const res = await getLinksSave('@linkCurto')
+
+            if(res.length === 0){
+
+            }
+            setMyLinks(res)
+        }
+        getLinks()
+    }, [])
+
     return(
         <div className='container-links'>
             <div className='Links-header'>
@@ -13,26 +32,17 @@ export default Links =>{
                 <h1>Meus Links</h1>
             </div>
 
-            <div className='links-item'>
-                <button className='link'>
-                    <FiLink size={18} color='#FFF' />
-                    http://pudim.com.br
-                </button>
-                <button className='link-delete'>
-                    <FiTrash size={24} color='#FF5454'/>
-                </button>
-            </div>
-
-            <div className='links-item'>
-                <button className='link'>
-                    <FiLink size={18} color='#FFF' />
-                    http://pudim.com.br
-                </button>
-                <button className='link-delete'>
-                    <FiTrash size={24} color='#FF5454'/>
-                </button>
-            </div>  
-            
+            {myLinks.map(link =>(
+                <div key={link.id} className='links-item'>
+                    <button className='link'>
+                        <FiLink size={18} color='#FFF' />
+                        {link.long_url}
+                    </button>
+                    <button className='link-delete'>
+                        <FiTrash size={24} color='#FF5454'/>
+                    </button>
+                </div>                
+            ))}            
         </div>
     )
 }
